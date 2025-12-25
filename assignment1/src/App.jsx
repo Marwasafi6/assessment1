17c240da3199a33091213e731de8d2dc14222100
Marwa9afi <220408900@st.biruni.edu.tr>
2025-12-25 21:37:46 +0300

    Add files via upload

---
+++ assignment1/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import ProductList from "./components/ProductList";
+
+function App() {
+  const [showProducts, setShowProducts] = useState(false);
+  const [name, setName] = useState("");
+
+  function toggleProducts() {
+    setShowProducts(!showProducts);
+  }
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>React Product Dashboard</h1>
+
+      <button onClick={toggleProducts}>
+        {showProducts ? "Hide Products" : "Show Products"}
+      </button>
+
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <p>Hello, {name}</p>
+
+      {showProducts && <ProductList />}
+    </div>
+  );
+}
+
+export default App;
